4e6045068682a457fd8d556cbca42df476b6597e
KihoGangaji <166824748+KihoGangaji@users.noreply.github.com>
2024-06-15 17:02:24 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,22 @@
+//体重変数の宣言・値の代入
+let weight = 68;
+
+//身長変数の宣言・値の代入
+let height = 1.7;
+
+//コンソールへの出力
+console.log(weight / (height * height));
+
+weight = 100;
+height = 1.8;
+console.log(weight / (height * height));
+
+weight = 'kkang';
+height = 'aji';
+console.log('weight' + 'height');
+console.log(weight + height);
+
+const kkangaji = 30;
+console.log(kkangaji);
+kkangaji= 80;
+console.log(kkangaji);
